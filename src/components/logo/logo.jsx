6d26c6894a377813ed2481611d25174a4e362efe
Anchal454 <shivianchal454@gmail.com>
2024-08-14 00:30:09 +0530

    changes

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,23 +1,12 @@
 import PropTypes from "prop-types";
 import { forwardRef } from "react";
-
-import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
-import { useTheme } from "@mui/material/styles";
 
 import { RouterLink } from "../../routes/components";
 
 // ----------------------------------------------------------------------
 
 const Logo = forwardRef(({ disabledLink = false, sx, ...other }, ref) => {
-  const theme = useTheme();
-
-  const PRIMARY_LIGHT = theme.palette.primary.light;
-
-  const PRIMARY_MAIN = theme.palette.primary.main;
-
-  const PRIMARY_DARK = theme.palette.primary.dark;
-
   const logo = (
     <h1
       ref={ref}
